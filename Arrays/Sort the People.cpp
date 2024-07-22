e3d3d2b4b46481b571b8f4e2fe1f99cc5fc19de2
ArunKathait <91815706+ArunKathait@users.noreply.github.com>
2024-07-22 09:32:58 +0530

    Create Sort the People.cpp

---
+++ Arrays/Sort the People.cpp
@@ -0,0 +1,36 @@
+
+class Solution {// TC--->O(NlogN)                   SC--->O(N)
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        // Create a vector of pairs to store heights and corresponding names
+        vector<pair<int,string>>vec;
+        
+        for(int i=0;i<heights.size();i++)
+        {
+            // Push each height and name pair into the vector
+            vec.push_back({heights[i],names[i]});
+        }
+        
+        // Sort the vector of pairs in descending order based on heights
+        sort(vec.begin(),vec.end(),greater<>());
+        
+        // Create a vector to store the sorted names
+        vector<string>ans;
+        
+        // Extract the names from the sorted vector of pairs
+        for(auto x:vec)
+        {
+            ans.push_back(x.second);
+        }
+        
+        // Return the sorted names
+        return ans;
+    }
+};
+
+/*
+NOTE:  If we have pair then we have to sort on both basis int or string then we write
+       greater<>() like this.We do not pass int inside it because we have to sort on both basis 
+       i.e(string,int).
+           
+*/

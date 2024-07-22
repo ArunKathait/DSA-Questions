0456e4df83611c3450b8c2de1d4b2a28550ff513
ArunKathait <91815706+ArunKathait@users.noreply.github.com>
2024-07-22 09:37:16 +0530

    Update Sort the People.cpp

--- Arrays/Sort the People.cpp
+++ Arrays/Sort the People.cpp
@@ -1,3 +1,4 @@
+****************************************APPROACH 1st*********************************************************
 
 class Solution {// TC--->O(NlogN)                   SC--->O(N)
 public:
@@ -34,3 +35,35 @@ NOTE:  If we have pair then we have to sort on both basis int or string then we 
        i.e(string,int).
            
 */
+
+*****************************************APPROACH 2nd**********************************************************
+
+
+class Solution {// TC--->O(NlogN)                   SC--->O(N)
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        // Create a vector of pairs to store heights and corresponding names
+        vector<pair<int,string>>vec;
+        
+        for(int i=0;i<heights.size();i++)
+        {
+            // Push each height and name pair into the vector
+            vec.push_back({heights[i],names[i]});
+        }
+        
+        // Sort the vector of pairs in descending order based on heights
+        sort(vec.rbegin(),vec.rend());
+        
+        // Create a vector to store the sorted names
+        vector<string>ans;
+        
+        // Extract the names from the sorted vector of pairs
+        for(auto x:vec)
+        {
+            ans.push_back(x.second);
+        }
+        
+        // Return the sorted names
+        return ans;
+    }
+};

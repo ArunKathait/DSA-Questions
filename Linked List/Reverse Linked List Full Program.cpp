071a06034446d738892290d653c1cb1a9b36f940
ArunKathait <91815706+ArunKathait@users.noreply.github.com>
2024-08-13 19:38:55 +0530

    Update Reverse Linked List Full Program.cpp

--- Linked List/Reverse Linked List Full Program.cpp
+++ Linked List/Reverse Linked List Full Program.cpp
@@ -1,4 +1,5 @@
-
+************************************************APPROACH 1st*****************************************************
+    
 #include<iostream>
 using namespace std;
 
@@ -57,3 +58,89 @@ int main()
     head = reverseList(head);
     printList(head);
 }
+
+
+************************************************APPROACH 2nd********************************************************
+
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node *next;
+    
+    Node(int new_data)
+    {
+        data = new_data;
+        next = NULL;
+    }
+};
+
+void printList(Node *head)
+{
+    Node *temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node* reverseList(Node *head)
+{
+    Node *temp = head;
+    Node *prev = NULL;
+    
+    while(temp != NULL)
+    {
+        Node *next = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = next;
+    }
+    return prev;
+}
+
+Node* createList(int n)
+{
+    if(n <= 0) return NULL;
+
+    int data;
+    cout << "Enter value for node 1: ";
+    cin >> data;
+    Node *head = new Node(data);
+    Node *current = head;
+
+    for(int i = 2; i <= n; i++)
+    {
+        cout << "Enter value for node " << i << ": ";
+        cin >> data;
+        current->next = new Node(data);
+        current = current->next;
+    }
+    
+    return head;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of nodes: ";
+    cin >> n;
+
+    Node *head = createList(n);
+
+    cout << "Before Reverse: ";
+    printList(head);
+    
+    head = reverseList(head);
+    
+    cout << "After Reverse: ";
+    printList(head);
+    
+    return 0;
+}
+  

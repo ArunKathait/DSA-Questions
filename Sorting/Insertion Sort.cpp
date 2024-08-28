e2d30b6e772078eefb6ff77bdf8c506a281a718f
ArunKathait <91815706+ArunKathait@users.noreply.github.com>
2024-08-28 22:00:36 +0530

    Create Insertion Sort.cpp

---
+++ Sorting/Insertion Sort.cpp
@@ -0,0 +1,35 @@
+
+#include<iostream>
+using namespace std;
+void insertionsort(int a[],int n)
+{
+   for(int i=0;i<n;i++)
+   {
+       int j = i;
+       while(j > 0 && a[j-1] > a[j])
+       {
+           int temp = a[j-1];
+           a[j-1] = a[j];
+           a[j] = temp;
+           j--;
+       }
+   }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+    insertionsort(a,n);
+    
+    for(auto x:a)
+    {
+        cout<<x<<" ";
+    }
+}
